97bf1cd2911a3e4fb618e7c0b1fe22d52865c139
Sophia Yarmolenko <59141170+SophiaYarmolenko@users.noreply.github.com>
2020-05-22 22:31:45 +0300

    Add vector of function

--- CLL/CLA.h
+++ CLL/CLA.h
@@ -8,10 +8,10 @@
 #include <iostream>;
 #include "History.h"
 #include "Command.h"
-
+#include <vector>
 using namespace std;
 
-class CLA:Command
+class CLA
 {
 private:
     History history;
@@ -19,15 +19,36 @@ private:
 
 public:
     CLA();
+
+    int numberFunction = 0;//number of function to form trie
+    typedef void (CLA::*Function)();
+    std::vector<Function> functions;
+
     void greeting();
     void help();
     void quit();
     void logOn();
-    void logOf();
+    void logOff();
     void logAppend();
     void logNew();
+    void Exit();
+    void save();
+    void load();
+    void logFile();
+    void list();
+    void logClearHistory();
     string TimeToString();
 };
 
 
 #endif //CLL_CLA_H
+
+
+
+
+/*
+    int i = 0;
+    (obj.*obj.functions[0])(i);
+    (obj.*obj.functions[1])(i);
+    (obj.*obj.functions[2])(i);
+*/
